7fa170928d82aab0ccdd8c7f7f0c201f93fbd87b
Muradamen <muradamen10@gmail.com>
2023-01-05 15:42:14 +0300

    allw

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Initalize a new square
+
+            Args:
+                size (int): The size of a new square
+        """
+        self.__size = size
