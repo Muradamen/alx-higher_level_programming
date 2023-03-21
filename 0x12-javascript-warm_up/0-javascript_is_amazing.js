8630d41ce701e79057d5b6cfbe039e2a528003a4
Muradamen <muradamen10@gmail.com>
2023-03-21 16:30:46 +0300

    javascript1

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
- #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
-=======
 #!/usr/bin/node
 
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
->>>>>>> 48c3bec394c4a7dec228f8454248863ce730b343
