eb866d5ce0b40d9f0dc91b4e509c7f33a367dacf
Muradamen <muradamen10@gmail.com>
2023-03-21 16:51:10 +0300

    argument1

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 
-const count = process.argv.length;
-if (count === 2) {
+if (process.argv.length === 2) {
     console.log('No argument');
-} else if (count === 3) {
+} else if (process.argv.length === 3) {
     console.log('Argument found');
 } else {
-<<<<<<< HEAD
-  console.log('Arguments found');
-} 
-=======
     console.log('Arguments found');
 }
->>>>>>> 2675e9df207133713da79896773770206615d7c5
