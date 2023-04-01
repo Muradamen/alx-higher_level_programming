fef2c17dac2c6e26ff74379e3b500eb7e0a65e03
Muradamen <muradamen10@gmail.com>
2023-03-31 19:46:29 -0700

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request to the URL and displays the value
+Script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
+
+Usage: ./1-hbtn_header.py <URL>
 """
 from sys import argv
 from urllib.request import Request, urlopen
