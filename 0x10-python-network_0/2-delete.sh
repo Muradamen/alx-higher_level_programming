710da299d6a80be4761378dd235b9eddc89a33a6
Muradamen <muradamen10@gmai.com>
2023-04-02 07:55:06 -0700

    delete

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Script that sends a DELETE request to a URL passed as the first argumen adn display the body ofresponse
-curl -sx DELETE "$1"
+# Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -sX DELETE "$1"
